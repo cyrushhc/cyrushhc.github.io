01457b728a8d013eb5fc97c5330edf8811c67092
莊驊軒 <cyrushhc@kens-MacBook-Pro.local>
2022-01-12 17:02:42 +0100

    debugging

--- myapp.py
+++ myapp.py
@@ -148,12 +148,12 @@ elif user_mode == "Participant":
     
     # For some reasons I cannot use the stream() method 
     # It says that 'CollectionReference' object has no attribute 'stream'
-    room_ref = db.collection(u'Room')
-    room_id_list = []
-    st.write(room_ref.stream())
-    for rooms in room_ref.stream():
-        room_id_list = [rooms.to_dict()['room_number']] 
-    room_number = st.number_input("Room Number:", value = 0)
+    # room_ref = db.collection(u'Room')
+    # room_id_list = []
+    # st.write(room_ref.stream())
+    # for rooms in room_ref.stream():
+    #     room_id_list = [rooms.to_dict()['room_number']] 
+    # room_number = st.number_input("Room Number:", value = 0)
 
 
 
