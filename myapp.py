38a027f1263f698011961ccc6f9e86a500ad8afd
莊驊軒 <cyrushhc@kens-MacBook-Pro.local>
2022-01-14 12:21:17 +0100

    Result Visualization

--- myapp.py
+++ myapp.py
@@ -152,24 +152,6 @@ if user_mode == "Admin":
                 st.write(doc['responses'])
     except:
         st.write("")    
-    
-
-
-
-button1 = st.empty()
-text1 = st.empty()
-button2 = st.empty()
-text2 = st.empty()
-
-ss = SessionState.get(button1 = False)
-
-if button1.button('1') :
-    ss.button1 = True
-
-if ss.button1:
-    text1.write('you clicked the first button')
-    if button2.button('2'):
-        text2.write('you clicked the second button')
 
 
 
