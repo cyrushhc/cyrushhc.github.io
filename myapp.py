8f00b76c22c6783191b2942413e6e4dd88b3e24e
Cyrus Chuang <cyrus@uni.minerva.edu>
2022-01-26 22:29:27 +0100

    Change

--- myapp.py
+++ myapp.py
@@ -284,6 +284,10 @@ elif user_mode == "Participant":
                 if doc['clustering_results'] == []:
                     st.write('There is no results yet. Check back later.')
                 else:
+                    st.write(doc['clustering_results'])
+                    st.write(doc['clustering_results'][0])
+                    st.write(type(doc['clustering_results'][0]))
+
                     for i in range(doc['clustering_results']):
                         st.write(f'### Cluster {i}')
                         st.write(list(dict.values(doc['clustering_results'][i])))
